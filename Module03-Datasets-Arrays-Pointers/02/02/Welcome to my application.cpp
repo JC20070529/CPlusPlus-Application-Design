68e25ed83b58d41bc5b785249626df32df14378a
JC20070529 <jianfengc565@gmail.com>
2026-09-09 12:22:25 +0800

    Add files via upload

---
+++ Module03-Datasets-Arrays-Pointers/02/02/Welcome to my application.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {	
+	//Main C++ variables type
+	string username = "JianFeng";//text
+	string ApplicationName = "Student Record Manager";//text
+	double version = 1.0;//decimal number
+	cout << "Welcome to my application！ " << endl;
+	cout << "Application Name: " << ApplicationName << endl;
+	cout << "user Name:" << username << endl;
+	cout << "Version nunmber: " << version << endl;
+system("pause>0");
+return 0;
+}
