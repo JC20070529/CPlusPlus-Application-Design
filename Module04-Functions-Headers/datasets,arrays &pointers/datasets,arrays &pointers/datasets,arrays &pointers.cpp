7de38dd40071185099304e4cc13060bd7838408c
JC20070529 <jianfengc565@gmail.com>
2026-09-16 03:59:36 +0800

    Add files via upload

---
+++ Module04-Functions-Headers/datasets,arrays &pointers/datasets,arrays &pointers/datasets,arrays &pointers.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+	{
+string names[5] ={
+		"Braund",
+		"Cumings",
+	    "Heikkinen",
+		"Futrelle",
+		"Allen"
+		};
+int ages[5] =
+		{
+		22,
+		38,
+		26,
+		35,
+		35
+		};
+double fares[5] =
+		{
+		7.25,
+		71.2833,
+		7.925,
+		53.10,
+		8.05
+		};
+
+double* farePtr = &fares[0];
+
+			cout << "Titanic Dataset Records" << endl;
+			for (int i = 0; i < 5; i++)
+			{
+			cout << names[i]<< ", Age: " << ages[i]<< ", Fare: $" << fares[i]<< endl;
+			}
+			cout << endl;
+			cout << "First fare through pointer: "<< *farePtr << endl;
+			return 0;
+}
